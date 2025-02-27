24560aa923150f08005ab3d80ccda6ecac9f1213
verok80 <yurasko1@yahoo.com>
2025-02-27 15:38:34 -0500

    Add files via upload

---
+++ src/FetchMeals.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+ const getAllMeals = (setMeal) => {
+
+    axios.get("https://meal-planner-backend-ksrk.onrender.com")
+    .then(({data}) => {console.log(data)
+        setMeal(data);
+})
+}
+const addMeal = (title, setTitle, setMeal) => {
+    axios.post("https://meal-planner-backend-ksrk.onrender.com/saveMeals", { title })
+    .then((data) => {
+        console.log(data)
+        setTitle("")
+        getAllMeals(setMeal)
+})
+}
+
+const editMeal = (mealId, title, setTitle,  setMeal, setEditing) => {
+    axios.post("https://meal-planner-backend-ksrk.onrender.com/editMeal", {_id: mealId, title })
+    .then((data) => {
+        console.log(data)
+        setTitle("")
+        setEditing(false)
+        getAllMeals(setMeal)
+})
+}
+
+const deleteMeal = (_id, setMeal) => {
+    axios.post("https://meal-planner-backend-ksrk.onrender.com/deleteMeal", { _id })
+    .then((data) => {
+        console.log(data)
+        getAllMeals(setMeal)
+})
+}
+
+
+export { getAllMeals, addMeal, editMeal, deleteMeal };
